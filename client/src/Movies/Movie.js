5a6a8b86a1c596a0f2f42a7da5506c5ae908b839
LaVon Mejia <lavonmejia@gmail.com>
2019-07-22 18:53:36 -0400

    mvp done

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+// import { Link } from "react-router-dom";
+
 
 const Movie = (props) => {
   const [movie, setMovie] = useState({});
  
   useEffect(() => {
-    const id = 2;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -32,6 +34,7 @@ const Movie = (props) => {
 
   const { title, director, metascore, stars } = movie;
   return (
+
     <div className="save-wrapper">
       <div className="movie-card">
         <h2>{title}</h2>
